934d2d71f6f88ac95b577b82799cb37d5494f9d8
Sitiepi <sitiepi7@gmail.com>
2026-01-19 09:09:16 +0700

    menambahkan konfigurasi firebase

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ import {
   doc,
   getDoc
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
+// GANTI DENGAN FIREBASE CONFIG ANDA
+const firebaseConfig = {
+  apiKey: "AIzaSyDgGanI0xfwbMbF2Q20eftio7Hc6iyPVgI",
+  authDomain: "insancemerlang-e9c87.firebaseapp.com",
+  projectId: "insancemerlang-e9c87",
+  storageBucket: "insancemerlang-e9c87.firebasestorage.app",
+  messagingSenderId: "1009245252263",
+  appId: "1:1009245252263:web:637bfe528eddfc0dc18982"
+};
